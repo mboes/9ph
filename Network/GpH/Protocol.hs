b1130775d97c4ccb46e1fd7493cd050b3cc6143e
Mathieu Boespflug <mboes@lix.polytechnique.fr>
2009-01-25 22:45:43 +0100

    Implement the message parsers.

--- Network/GpH/Protocol.hs
+++ Network/GpH/Protocol.hs
@@ -1,15 +1,17 @@
 -- | Parse a stream of requests and dump a stream of responses.
 module Network.GpH.Protocol (decode, encode) where
 
-import Data.ByteString
+import qualified Data.ByteString as B
 import Network.GpH.Types
 import Data.Maybe (fromJust)
 import Data.Bits
 import Data.Word
-import Control.Monad (forever)
+import qualified Data.Map as Map
+import Control.Monad
+import Prelude hiding (read)
 
 
-newtype Dec a = Dec { unDec :: ByteString -> (a, ByteString) }
+newtype Dec a = Dec { unDec :: B.ByteString -> (a, B.ByteString) }
 
 instance Monad Dec where
     return x = Dec $ \s -> (x, s)
@@ -17,35 +19,57 @@ instance Monad Dec where
 
 
 -- Message codes, from 9p.h in Plan9 implementation of 9p.
-msgCodes = Map.fromList $ zipWith (filter even [0..])
+msgCodes = Map.fromList $ zip (filter even [0..])
            [ version, auth, attach, undefined, flush, walk, open
            , create, read, write, clunk, remove, stat, wstat ]
 
-decode :: ByteString -> [Request]
+decode :: B.ByteString -> [Request]
 decode = fst . unDec (forever request)
 
-encode :: [Reply] -> ByteString
+encode :: [Reply] -> B.ByteString
 encode = undefined
 
-request :: Dec [Request]
+request :: Dec Request
 request = do
   size <- word32
   req  <- byte
   tag  <- word16
-  Map.lookup req msgCodes
+  msgCodes Map.! req
+
+version = return Tversion `ap` arbitrary
+auth    = return Tauth `ap` word32 `ap` arbitrary `ap` arbitrary
+flush   = return Tflush `ap` word16
+attach  = return Tattach `ap` word32 `ap` word32 `ap` arbitrary `ap` arbitrary
+walk    = do f <- return Twalk `ap` word32 `ap` word32
+             count <- word16
+             return f `ap` replicateM (fromIntegral count) arbitrary
+open    = return Topen `ap` word32 `ap` byte
+create  = return Tcreate `ap` word32 `ap` arbitrary `ap` word32 `ap` byte
+read    = return Tread `ap` word32 `ap` word64 `ap` word32
+write   = do f <- return Twrite `ap` word32 `ap` word64
+             count <- word32
+             return f `ap` bytes (fromIntegral count)
+clunk   = return Tclunk `ap` word32
+remove  = return Tremove `ap` word32
+stat    = return Tstat `ap` word32
+wstat   = return Twstat `ap` word32 `ap` arbitrary
 
 -- Combinators
 
-byte = Dec $ fromJust . uncons
-bytes n = Dec $ \bs -> splitAt n bs
+byte = Dec $ fromJust . B.uncons
+bytes n = Dec $ \bs -> B.splitAt n bs
+
+-- | The number of bytes to parse is given in the first two bytes of the input.
+arbitrary = word16 >>= \n -> Dec $ \bs -> B.splitAt (fromIntegral n) bs
 
 word16 :: Dec Word16
 word16 = do x1 <- byte; x2 <- byte
             return $ shiftL (fromIntegral x1) 8 .|. fromIntegral x2
 
 word32 :: Dec Word32
-word32 = do x1 <- byte; x2 <- byte; x3 <- byte; x4 <- byte
-            return $  shiftL (fromIntegral x1) 24
-                  .|. shiftL (fromIntegral x2) 16
-                  .|. shiftL (fromIntegral x3) 8
-                  .|.         fromIntegral x4
+word32 = do x1 <- word16; x2 <- word16
+            return $ shiftL (fromIntegral x1) 16 .|. fromIntegral x2
+
+word64 :: Dec Word64
+word64 = do x1 <- word32; x2 <- word32
+            return $ shiftL (fromIntegral x1) 32 .|. fromIntegral x2
