4d97fdc41638bf123036281e032dbdc3a505d8e6
Mathieu Boespflug <mboes@lix.polytechnique.fr>
2009-01-29 22:09:32 +0100

    First stab at deriving decoding/encoding protocol.
    Size calculation is slightly wrong, decoding/encoding functions don't
    accept tags, and request codes and reply codes are wrong. Also, only
    'encode' is implemented, no 'decode' yet.

--- src/Network/GpH/Protocol.hs
+++ src/Network/GpH/Protocol.hs
@@ -1,56 +1,20 @@
 -- | Parse a stream of requests and dump a stream of responses.
 module Network.GpH.Protocol (decode, encode) where
 
-import qualified Data.ByteString.Lazy as B
+import Network.GpH.Protocol.Derive
 import Network.GpH.Types
-import Control.Monad
+import Foreign.Storable
+import Data.Binary hiding (decode, encode)
 import Data.Word
-import Data.Array.IArray
-import Data.Data
-import Data.Generics.Aliases
-import Data.Binary
-import Foreign.Storable (sizeOf)
-import Prelude hiding (read)
+import qualified Data.ByteString as B
 
+instance Storable B.ByteString where
+    alignment = undefined
+    sizeOf = B.length
 
-size :: (Data d, Num n) => d -> n
-size = gmapQl (+) 0 dispatch where
-    dispatch :: (Data d, Num n) => d -> n
-    dispatch = error "Impossible"
-               `extQ` word16 `extQ` word32 `extQ` word64
-               `extQ` arbitrary `extQ` list arbitrary
-    word16 x = fromIntegral $ sizeOf (x :: Word16)
-    word32 x = fromIntegral $ sizeOf (x :: Word32)
-    word64 x = fromIntegral $ sizeOf (x :: Word64)
-    arbitrary x = fromIntegral $ B.length (x :: B.ByteString)
-    list f = sum . map f
+instance Storable [a] where
+    alignment = undefined
+    sizeOf = length
 
-reply :: Put
-reply = undefined
+$(derive (undefined :: Request))
 
-request :: Get Request
-request = do
-  size <- word32
-  req  <- getWord8
-  tag  <- word32
-  fromConstrM dispatch (lookupConstr req)
-    where dispatch :: Data d => Get d
-          dispatch = error "Impossible"
-                     `extR` word16 `extR` word32 `extR` word64
-                     `extR` arbitrary `extR` list arbitrary
-
-lookupConstr = let constructors :: Array Int Constr
-                   constructors =
-                       listArray (0, 14) $ dataTypeConstrs
-                                     $ dataTypeOf (undefined :: Request)
-               in \i -> constructors ! ((fromIntegral i - 100) `div` 2)
-
--- Combinators
-
--- | The number of bytes to parse is given in the first two bytes of the input.
-arbitrary = word16 >>= \n -> replicateM (fromIntegral n) getWord8
-list m = word16 >>= \n -> replicateM (fromIntegral n) m
-
-word16 = get :: Get Word16
-word32 = get :: Get Word32
-word64 = get :: Get Word64
---
+++ src/Network/GpH/Protocol/Derive.hs
@@ -0,0 +1,68 @@
+module Network.GpH.Protocol.Derive (derive) where
+
+import Language.Haskell.TH
+import Data.Binary
+import Foreign.Storable
+import Data.Generics
+import Control.Monad
+
+
+-- size :: (Data d, Num n) => d -> n
+-- size = gmapQl (+) 0 dispatch where
+--     dispatch :: (Data d, Num n) => d -> n
+--     dispatch = error "Impossible"
+--                `extQ` word16 `extQ` word32 `extQ` word64
+--                `extQ` arbitrary `extQ` list arbitrary
+--     word16 x = fromIntegral $ sizeOf (x :: Word16)
+--     word32 x = fromIntegral $ sizeOf (x :: Word32)
+--     word64 x = fromIntegral $ sizeOf (x :: Word64)
+--     arbitrary x = fromIntegral $ B.length (x :: B.ByteString)
+--     list f = sum . map f
+
+derive :: Data a => a -> Q [Dec]
+derive x = sequence [storableInst, decodeD, encodeD]
+    where storableInst = do
+            methods <- sequence [sizeOfD, alignmentD]
+            return $ InstanceD []
+                       (AppT (ConT (mkName "Storable"))
+                                 (ConT (mkName (typeName x)))) methods
+          alignmentD = liftM head [d| alignment = undefined |]
+          sizeOfD = do
+            clauses <- mapM sizeClause constructors
+            return $ decl "sizeOf" clauses
+          decodeD = liftM head [d| decode = undefined |]
+          encodeD = do
+            clauses <- mapM putClause constructors
+            return $ decl "encode" clauses
+          typeName x = tyConString $ fst $ splitTyConApp $ typeOf x
+          constructors = zip [(0 :: Int)..] $ map gen $ dataTypeConstrs $ dataTypeOf x
+          gen con = ( mkName (showConstr con)
+                    , gmapQ (\x -> do n <- newName "x"
+                                      return (n, typeName x))
+                                 $ fromConstr con `asTypeOf` x
+                    )
+          sizeClause (n, (con, argsm)) = do
+            args <- sequence argsm
+            c <- newName "c"
+            let pat = AsP c (ConP con (map (VarP . fst) args))
+            return $ Clause [pat] (NormalB (AppE (var "sum") (ListE (map sizeOf args)))) []
+              where sizeOf (arg, _) = AppE (var "sizeOf") (VarE arg)
+          putClause (n, (con, argsm)) = do
+            args <- sequence argsm
+            c <- newName "c"
+            let pat = AsP c (ConP con (map (VarP . fst) args))
+            return $ Clause [pat] (NormalB (DoE (putsizeOfS c : putreqCodeS n : map gputS args))) []
+          putsizeOfS c = NoBindS $ putE $ AppE (var "sizeOf") (VarE c)
+          putreqCodeS n = NoBindS $ putE (SigE (LitE (IntegerL (fromIntegral n))) (ConT (mkName "Word32")))
+          gputS (arg, ty) =
+              NoBindS $ case ty of
+                          "Data.ByteString.Internal.ByteString" -> putByteStringS arg
+                          "Prelude.[]" -> putByteStringListS arg
+                          _ -> putE (VarE arg)
+          putByteStringS x =
+              DoE [ NoBindS $ putE $ AppE (var "sizeOf") (VarE x)
+                  , NoBindS $ putE $ (VarE x) ]
+          putByteStringListS = undefined
+          var = VarE . mkName
+          decl x clauses = FunD (mkName x) clauses
+          putE = AppE (VarE (mkName "put"))
