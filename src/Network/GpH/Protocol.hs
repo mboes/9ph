f9fe5f3715f9adc12e40b62668c66d16aee8eeca
Mathieu Boespflug <mboes@lix.polytechnique.fr>
2009-02-02 01:58:01 +0100

    Remove Protocol class.
    Now Protocol.hs only defines Binary instances, some of them written
    manually, those that can be derived from Types.hs are generated via
    Template Haskell. Namely Qid, Request and Reply.

--- src/Network/GpH/Protocol.hs
+++ src/Network/GpH/Protocol.hs
@@ -3,18 +3,44 @@ module Network.GpH.Protocol (decode, encode) where
 
 import Network.GpH.Protocol.Derive
 import Network.GpH.Types
-import Data.Binary hiding (decode, encode)
+import Data.Binary
 import Data.Binary.Put
 import Data.Binary.Get
 import Data.Word
+import Control.Monad (liftM)
 import qualified Data.ByteString.Lazy.Char8 as B
+import Data.BitSet (BitSet)
+import qualified Data.BitSet as BitSet
 
 
-class Protocol a where
-    size   :: a -> Word32
-    decode :: B.ByteString -> a
-    encode :: a -> B.ByteString
+instance Binary (BitSet QType) where
+    get = do w :: Word8 <- get
+             return BitSet.coerceTo w
+    put bs = put (BitSet.coerceFrom bs :: Word8)
 
+instance Binary (BitSet Permission) where
+    get = do w :: Word32 <- get
+             return BitSet.coerceTo w
+    put bs = put (BitSet.coerceFrom bs :: Word32)
+
+instance Binary (UField Info) where
+    get = do len :: Word16 <- get
+             liftM UF $ getLazyByteString (fromIntegral len)
+    put (UF str) = do put (fromIntegral (B.length str) :: Word16)
+                      putLazyByteString str
+
+instance Binary (UField Name) where
+    get = do len :: Word16 <- get
+             liftM UF $ getLazyByteString (fromIntegral len)
+    put (UF str) = do put (fromIntegral (B.length str) :: Word16)
+                      putLazyByteString str
+
+instance Binary (UField FData) where
+    get = do len :: Word32 <- get
+             liftM UF $ getLazyByteString (fromIntegral len)
+    put (UF str) = do put (fromIntegral (B.length str) :: Word32)
+                      putLazyByteString str
+
+$(derive (undefined :: Qid))
 $(derive (undefined :: Request))
-
 $(derive (undefined :: Reply))
